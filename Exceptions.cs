5c5e1791895572cb02c96dfc71521b273faeea85
teodoratsaneva <teodora.tsaneva@p3-group.com>
2023-08-31 18:16:34 +0300

    adding new solutions

--- Exceptions.cs
+++ Exceptions.cs
@@ -36,68 +36,68 @@ public class Program
         throw new ArgumentOutOfRangeException();
     }
 
-    // public static void Main(string[] args)
-    // {
+    public static void Main(string[] args)
+    {
 
-    //     //TASK 1
-    //     double number;
+        //TASK 1
+        double number;
 
-    //     if (double.TryParse(Console.ReadLine(), out number))
-    //     {
-    //         double result;
+        if (double.TryParse(Console.ReadLine(), out number))
+        {
+            double result;
 
-    //         try
-    //         {
-    //             result = GetSquareRoot(number);
-    //             Console.WriteLine(result);
-    //         }
-    //         catch (InvalidOperationException)
-    //         {
-    //             Console.WriteLine("Invalid number");
-    //         }
+            try
+            {
+                result = GetSquareRoot(number);
+                Console.WriteLine(result);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Invalid number");
+            }
 
-    //         Console.WriteLine("Good bye");
-    //     }
-    //     else
-    //     {
-    //         Console.WriteLine("Invalid input. Please enter a valid number.");
-    //     }
+            Console.WriteLine("Good bye");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
 
-    //     //TASK 2
-    //     int[] array = new int[10];
+        //TASK 2
+        int[] array = new int[10];
 
-    //     for (int i = 0; i < 10; i++)
-    //     {
-    //         int number = int.Parse(Console.ReadLine());
-    //         try
-    //         {
-    //             if (IsValidNumber(number))
-    //             {
-    //                 array[i] = number;
-    //             }
-    //         }
-    //         catch (ArgumentOutOfRangeException)
-    //         {
-    //             Console.WriteLine("Invalid input.");
-    //         }
-    //     }
+        for (int i = 0; i < 10; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            try
+            {
+                if (IsValidNumber(number))
+                {
+                    array[i] = number;
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
 
-    //     try
-    //     {
-    //         array = CheckArray(array);
+        try
+        {
+            array = CheckArray(array);
 
-    //         Console.Write("1 < ");
+            Console.Write("1 < ");
 
-    //         foreach (int number in array)
-    //         {
-    //             Console.Write($"{number} < ");
-    //         }
+            foreach (int number in array)
+            {
+                Console.Write($"{number} < ");
+            }
 
-    //         Console.Write("100");
-    //     }
-    //     catch(InvalidOperationException)
-    //     {
-    //         Console.WriteLine("Exception");
-    //     }
-    // }
+            Console.Write("100");
+        }
+        catch(InvalidOperationException)
+        {
+            Console.WriteLine("Exception");
+        }
+    }
 }
